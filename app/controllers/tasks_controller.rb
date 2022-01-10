4d2cc0d355d32a24dafa26159afec9c0929018f7
kayama <kohei.yamacguhi05.study@gmail.com>
2022-01-10 08:00:14 +0000

    add login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,14 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
     @task = Task.find(params[:id])
+    if !current_user.tasks.find_by(user_id: @task.user_id)
+      redirect_to root_url
+    end
   end
 
   def new
@@ -12,7 +16,8 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
+    
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
